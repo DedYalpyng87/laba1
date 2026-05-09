e7163e285210cb0c2bf9963cb1795e3840c5be3d
Гузаревич Роман Алексеевич, гр.250541 <juvik290887@mail.ru>
2026-05-09 11:17:34 +0300

    Добавлены новые операции Complex и использованы в уравнении фрактала

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -70,6 +70,25 @@ public class Complex {
         im += b.im;
         return this;
     }
+        /**
+     * Subtract operation.
+     * @param b subtrahend
+     * @return this Complex object whose value is (this - b)
+     */
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    /**
+     * Conjugate operation.
+     * @return this Complex object conjugated
+     */
+    public Complex conjugate() {
+        im = -im;
+        return this;
+    }
 
     /**
      * Multiply operation.
